ec0a8e9451d4e23205eea6799637063f640823eb
flux <25628292+fluxionary@users.noreply.github.com>
2023-03-19 16:54:40 -0700

    change name of API to what the code sez everywhere else

--- api.lua
+++ api.lua
@@ -1,6 +1,6 @@
 status_effects.registered_effects = {}
 
-function status_effects.create_effect(name, def)
+function status_effects.register_effect(name, def)
 	local effect = status_effects.StatusEffect(name, def)
 	status_effects.registered_effects[name] = effect
 	return effect
