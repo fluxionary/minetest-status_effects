6e777c5bf00642e49d9e4b8b1993585ece2b7b72
flux <25628292+fluxionary@users.noreply.github.com>
2023-03-29 12:53:04 -0700

    fix elapsed callback value in globalstep

--- globalstep.lua
+++ globalstep.lua
@@ -1,52 +1,47 @@
 local last_step_by_effect_name = {}
 
-local function process_step_every_catch_up(players, effect, dtime, now, elapsed)
+local function update_remainings(player, meta, effect, remainings, elapsed)
+	for key, remaining_time in pairs(remainings) do
+		if remaining_time > elapsed then
+			remainings[key] = remaining_time - elapsed
+		else
+			remainings[key] = nil
+			effect._monoid:del_change(player, key)
+		end
+	end
+
+	effect:_set_remainings(meta, remainings)
+end
+
+local function process_step_every_catch_up(players, effect, now, elapsed)
 	for i = 1, #players do
 		local player = players[i]
 		local meta = player:get_meta()
 		local remainings = effect:_get_remainings(meta)
 
 		local value = effect:value(player)
-		effect:on_step(player, value, dtime, now)
+		effect:on_step(player, value, effect.step_every, now)
 
 		for _ = 1, math.floor(elapsed / effect.step_every) do
-			for key, remaining_time in pairs(remainings) do
-				if remaining_time > effect.step_every then
-					remainings[key] = remaining_time - effect.step_every
-				else
-					remainings[key] = nil
-					effect._monoid:del_change(player, key)
-				end
-			end
+			update_remainings(player, meta, effect, remainings, effect.step_every)
 
-			effect:on_step(player, effect:value(player), 0, now)
+			effect:on_step(player, effect:value(player), effect.step_every, now)
 		end
-
-		effect:_set_remainings(meta, remainings)
 	end
 
 	return elapsed % effect.step_every
 end
 
-local function process_step(players, effect, dtime, now)
+local function process_step(players, effect, now, elapsed)
 	for i = 1, #players do
 		local player = players[i]
 		local meta = player:get_meta()
 		local remainings = effect:_get_remainings(meta)
 
 		local value = effect:value(player)
-		effect:on_step(player, value, dtime, now)
+		effect:on_step(player, value, elapsed, now)
 
-		for key, remaining_time in pairs(remainings) do
-			if remaining_time > dtime then
-				remainings[key] = remaining_time - dtime
-			else
-				remainings[key] = nil
-				effect._monoid:del_change(player, key)
-			end
-		end
-
-		effect:_set_remainings(meta, remainings)
+		update_remainings(player, meta, effect, remainings, elapsed)
 	end
 end
 
@@ -60,16 +55,16 @@ minetest.register_globalstep(function(dtime)
 				local elapsed = (last_step_by_effect_name[effect_name] or 0) + dtime
 				if elapsed >= effect.step_every then
 					if effect.step_catchup then
-						elapsed = process_step_every_catch_up(players, effect, dtime, now, elapsed)
+						elapsed = process_step_every_catch_up(players, effect, now, elapsed)
 					else
-						process_step(players, effect, dtime, now)
+						process_step(players, effect, now, elapsed)
 						elapsed = 0
 					end
 				end
 
 				last_step_by_effect_name[effect_name] = elapsed
 			else
-				process_step(players, effect, dtime, now)
+				process_step(players, effect, now, dtime)
 			end
 		end
 	end
